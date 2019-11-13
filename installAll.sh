a1b246d6b10c0e62d86091b53b390642f0cf5fa7
GMouaad <Mgsair@gmail.com>
2019-11-13 23:01:30 +0100

    Changed the install scripts and added telegraf.conf

--- installAll.sh
+++ installAll.sh
@@ -4,8 +4,22 @@
 # Author: Mouaad Gssair, M.Gssair@htwg-konstanz.de
 # NOTE: Make sure to run this script with root access
 
+set -e
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
 # Install Docker
-curl -sSL https://get.docker.com | sh
+# Test if Docker is already installed
+echo "Cheking if Docker is installed.."
+docker --version 2>&1 >/dev/null # 
+DOCKER_IS_AVAILABLE=$?
+# If return value is 0, then it is installed
+if [ $DOCKER_IS_AVAILABLE -eq 0 ]; then
+    echo "Docker already installed"
+else
+    echo "Docker is not installed"
+    echo "Installing Docker"
+    curl -sSL https://get.docker.com | sh
+fi
 
 # Configure docker to run as non-root user
 sudo groupadd docker
@@ -13,12 +27,23 @@ sudo usermod -aG docker $USER
 newgrp docker
 
 # Install Docker-compose
+echo "Installing Docker-compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 # Set Permissions
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Verify Installation
-docker-compose --version
+# Test if Installation was successful
+echo "Cheking if Installation was successful"
+docker-compose --version 2>&1 >/dev/null # Output is not shown on the console
+COMPOSE_IS_AVAILABLE=$?
+# If return value is 0, then it is installed
+if [ $COMPOSE_IS_AVAILABLE -eq 0 ]; then
+    echo "Docker-compose installed successfully"
+else
+    echo "Docker-compose was not installed correctly"
+fi
 
-docker build -f service_dockerfile -t title
+
+# Build All the docker images from their docker file
+#docker build -f service_dockerfile -t title
---
+++ install_Infrastructure.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# This Script is meant for the Stack Deployment of the HTWG IoT Infrastructur Solution
+# Author: Mouaad Gssair, M.Gssair@htwg-konstanz.de
+# NOTE: Make sure to run this script with root access
+# TODO: Proxy Settings ?
+
+set -e
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
+# Test if Git is already installed
+echo "Cheking if Git is installed.."
+git --version 2>&1 >/dev/null # 
+GIT_IS_AVAILABLE=$?
+# If return value is 0, then it is installed
+if [ $GIT_IS_AVAILABLE -eq 0 ]; then
+    echo "Git already installed"
+else
+    echo "Git is not installed"
+    echo "Installing Git"
+    sudo apt-get install git
+fi
+
+mkdir ~/IoTstack
+cd  ~/IoTstack
+# Initializing and cloning Repository
+git init
+git remote add origin -f https://github.com/GMouaad/HTWG_IoT_Solution.git
+git config core.sparseCheckout true
+echo "/Infrastructure" > .git/info/sparse-checkout
+git pull origin master
+
+# Creating Docker Volumes and Network for Metrics Monitoring
+cd Infrastructure/Monitoring
+docker network create monitoring
+docker volume create grafana-volume
+docker volume create influxdb-volume
+
+# Cheking if the creation was successful
+docker network ls | grep 'monitoring' &> /dev/null
+if [ $? == 0 ]; then
+   echo "Network created successfully"
+fi
+
+docker volume ls | grep 'grafana-volume' &> /dev/null
+if [ $? == 0 ]; then
+   echo "Volumes created successfully"
+fi
+
+# Runing the containers to create the config files and removing them afterwards
+docker run --rm \
+  -e INFLUXDB_DB=telegraf -e INFLUXDB_ADMIN_ENABLED=true \
+  -e INFLUXDB_ADMIN_USER=admin \ # change User!
+  -e INFLUXDB_ADMIN_PASSWORD=supersecretpassword \ # change Password!
+  -e INFLUXDB_USER=telegraf -e INFLUXDB_USER_PASSWORD=secretpassword \ # change user and password!
+  -v influxdb-volume:/var/lib/influxdb \
+  influxdb /init-influxdb.sh
